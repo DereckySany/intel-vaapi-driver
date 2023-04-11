f682b76611ec31afc8c40eba74e6ad856d1f8699
DereckySany <leirecky@gmail.com>
2023-04-11 19:20:21 -0300

    Update intel_gen_vppapi.h

--- src/intel_gen_vppapi.h
+++ src/intel_gen_vppapi.h
@@ -35,7 +35,7 @@
 /* #include <i965_surface.h> */
 #include <i965_post_processing.h>
 
-static inline VAStatus
+inline VAStatus
 intel_common_scaling_post_processing(VADriverContextP ctx,
                                      struct i965_post_processing_context *pp_context,
                                      const struct i965_surface *src_surface,
